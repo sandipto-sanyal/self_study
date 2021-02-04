23d5435e841303f2a1c1f7c515fdff9abb8203dd
sandipto-sanyal <44098866+sandipto-sanyal@users.noreply.github.com>
2021-02-04 15:40:56 +0530

    pandas parallel apply final

--- parallel_processing/pandas_parallel_apply.py
+++ parallel_processing/pandas_parallel_apply.py
@@ -10,7 +10,16 @@ import numpy as np
 import re
 from multiprocessing import Pool,cpu_count
 
-def map_sentence_with_stw(sentence):
+def map_sentence_with_stw(row):
+    # read the taxonomy file
+    taxonomy_file_path = r'C:\Users\sandipto.sanyal\Documents\AVS PS\TSA VIP\phase2\data\Intent and Guidance captures\CRs\taxonomy.csv'
+    taxonomy_df = pd.read_csv(taxonomy_file_path,
+                              header=None,
+                              index_col=0
+                             )
+    trunc_taxonomy_df = taxonomy_df[~(taxonomy_df.index=='intent')]
+    index = row.name
+    sentence = row.original_sentence
     sentences = np.array(re.split(r'\. |\? |\! |\n+',sentence.strip()), dtype='object')
     stws_found = []
     for mtw in trunc_taxonomy_df.index:
@@ -24,7 +33,7 @@ def map_sentence_with_stw(sentence):
         # return only the STWs found in the sentence
         stws_found.extend(sentence_stw_pair[:,0])
     # return number of STWs found alongwith count
-    print('Done::: ', sentence)
+    print(index, ') Done::: ', sentence)
     return pd.Series([', '.join(stws_found), len(stws_found)])
 
 def custom_apply_function(row):
@@ -34,7 +43,7 @@ def custom_apply_function(row):
     return pd.Series([len(sentence), 1])
 
 def perform_apply_on_df_chunks(chunk_df):
-    chunk_df[['stws_found_on_full_scan','number_of_stws']] = chunk_df.apply(custom_apply_function, axis=1)
+    chunk_df[['stws_found_on_full_scan','number_of_stws']] = chunk_df.apply(map_sentence_with_stw, axis=1)
     return chunk_df
 
 def parallelize_dataframe(df, func, n_cores=cpu_count()-3):
@@ -49,15 +58,9 @@ def parallelize_dataframe(df, func, n_cores=cpu_count()-3):
 
 # as per the doc: Make sure that the main module can be safely imported by a new Python interpreter without causing unintended side effects (such a starting a new process)
 if __name__ == '__main__':
-    taxonomy_file_path = r'C:\Users\sandipto.sanyal\Documents\AVS PS\TSA VIP\phase2\data\Intent and Guidance captures\CRs\taxonomy.csv'
-    taxonomy_df = pd.read_csv(taxonomy_file_path,
-                              header=None,
-                              index_col=0
-                             )
-    trunc_taxonomy_df = taxonomy_df[~(taxonomy_df.index=='intent')]
     training_dataset_path = r'C:\Users\sandipto.sanyal\Documents\AVS PS\TSA VIP\phase2\data\Intent and Guidance captures\CRs\training_data.xlsx'
     training_df = pd.read_excel(training_dataset_path)
     print('Training df loaded ', training_df.shape)
-    final_df = parallelize_dataframe(training_df.head(100),perform_apply_on_df_chunks)
+    final_df = parallelize_dataframe(training_df,perform_apply_on_df_chunks)
     save_path = r'C:\Users\sandipto.sanyal\Documents\AVS PS\TSA VIP\phase2\data\Intent and Guidance captures\CRs\training_data_with_mapped_stw.xlsx'
     final_df.to_excel(save_path, index=False)
